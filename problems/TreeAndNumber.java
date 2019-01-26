652753e46505ab59e7672ce87da689d0c0201a84
WFC <0922bkk@gmail.com>
2019-01-25 23:32:25 -0800

    Tree and number

---
+++ problems/TreeAndNumber.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+public class TreeAndNumber {
+  public static void main(String[] args) {
+
+  }
+
+  class Node {
+    Node left;
+    Node right;
+    int val;
+
+    public Node(int val) {
+      this.val = val;
+    }
+  }
+
+
+}
