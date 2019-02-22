4b002b00a43849a8e432be26c8a0a1449dff2015
Fengchao Wang <fwang@elementum.com>
2019-02-22 14:51:35 -0800

    Singleton and sockets

---
+++ src/main/java/io/fengchao/applets/socket/EchoClient.java
@@ -0,0 +1,22 @@
+package io.fengchao.applets.socket;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class EchoClient {
+  public static void main(String[] args) throws UnknownHostException, IOException {
+    if (args.length != 2) {
+      System.err.println(
+          "Usage: java EchoClient <host name> <port number>");
+      System.exit(1);
+    }
+    String hostName = args[0];
+    int portNumber = Integer.parseInt(args[1]);
+    try (
+        Socket echoSocket = new Socket(hostName, portNumber);
+        ) {
+
+    }
+  }
+}
