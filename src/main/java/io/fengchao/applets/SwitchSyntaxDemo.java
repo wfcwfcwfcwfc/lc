a1720e84441e61cf5c2ebda7d686ce3df6526c93
WFC <0922bkk@gmail.com>
2019-02-23 15:54:21 -0800

    Switch syntax demo

---
+++ src/main/java/io/fengchao/applets/SwitchSyntaxDemo.java
@@ -0,0 +1,22 @@
+package io.fengchao.applets;
+
+import java.util.Scanner;
+
+public class SwitchSyntaxDemo {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String in = sc.nextLine();
+
+    switch(in) {
+      case "w":
+        System.out.println("Wang");
+        break;
+      case "f":
+        System.out.println("Fengchao");
+        break;
+      default:
+          System.out.println("OK");
+    }
+
+  }
+}
