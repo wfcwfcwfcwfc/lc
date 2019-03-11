4ca0ab1ed400caf146fb080c27e9aa0146740ce3
Fengchao Wang <fwang@elementum.com>
2019-03-10 21:20:46 -0700

    Array and streams

---
+++ src/main/java/io/fengchao/java/Streams.java
@@ -0,0 +1,32 @@
+package io.fengchao.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Streams {
+  public static void main(String[] args) {
+    Person person1 = new Person();
+    person1.value = "Fengchao";
+    Person person2 = new Person();
+    person2.value = "Wang";
+
+    List<Person> list = new ArrayList<Person>();
+    list.add(person1);
+    list.add(person2);
+    list.stream().forEach(new Consumer<Person>() {
+      @Override
+      public void accept(Person person) {
+        person.value = "ppp2";
+      }
+    });
+    list.forEach(person -> {
+      System.out.println(person.value);
+    });
+  }
+}
+
+class Person {
+  public String value;
+}
