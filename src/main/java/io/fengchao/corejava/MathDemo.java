3121ecbe87ebc07e8acc0c1112bf8e22c90c4827
WFC <0922bkk@gmail.com>
2020-05-13 13:12:32 -0500

    Practice the equals function

--- src/main/java/io/fengchao/corejava/MathDemo.java
+++ src/main/java/io/fengchao/corejava/MathDemo.java
@@ -1,8 +1,25 @@
 package io.fengchao.corejava;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MathDemo {
   public static void main(String[] args) {
     int fmod = Math.floorMod(-3, 8);
     System.out.println(fmod);
+
+    Map<Character, Integer> pmap = new HashMap<>();
+    Map<Character, Integer> smap = new HashMap<>();
+
+    pmap.put('a', 1);
+    pmap.put('b', 2);
+    pmap.put('c', 3);
+
+    pmap.equals(smap);
+    smap.put('a', 1);
+    pmap.equals(smap);
+    smap.put('b', 2);
+    smap.put('c', 3);
+    pmap.equals(smap);
   }
 }
