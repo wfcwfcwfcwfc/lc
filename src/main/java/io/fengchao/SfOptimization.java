7dec4235143f5852118df34db8c0fd8918dde0e3
Fengchao Wang <fengchao_wang@apple.com>
2022-03-30 14:01:28 -0700

    Weighted app ranking in SF migration

--- src/main/java/io/fengchao/SfOptimization.java
+++ src/main/java/io/fengchao/SfOptimization.java
@@ -17,17 +17,18 @@ public class SfOptimization {
     }
 
     public static void solution() throws IOException {
-        Path path = Paths.get("/Users/fengchaowang/Downloads/result.csv");
+        Path path = Paths.get("/Users/fengchaowang/Downloads/result-2.csv");
 
 
         List<String[]> read = Files.readAllLines(path)
                 .stream().map(row -> row.split(","))
-                .map(row-> new String[]{row[2], row[4]}).collect(Collectors.toList());
+                .map(row-> new String[]{row[6], row[8], row[0]}).collect(Collectors.toList());
 
         Map<String, List<String>> map = new HashMap<>();
-
+        Map<String, Integer> appToCpu = new HashMap<>();
         for(String[] pair : read) {
             map.computeIfAbsent(pair[0], k -> new ArrayList<String>()).add(pair[1]);
+            appToCpu.put(pair[0], Integer.valueOf(pair[2].replaceAll("\\D+", "")));
         }
 
 
@@ -38,19 +39,19 @@ public class SfOptimization {
             list.addAll(set);
         });
 
-        calculateShortestPathFromSource(map);
+        calculateShortestPathFromSource(map, appToCpu);
 
 
     }
 
-    public static void calculateShortestPathFromSource(Map<String, List<String>> graph) {
+    public static void calculateShortestPathFromSource(Map<String, List<String>> graph, Map<String, Integer> weights) {
 
         List<String> processedDeps = new ArrayList<>();
         List<String> processedApps = new ArrayList<>();
         List<Integer> length = new ArrayList<Integer>();
 
         while (graph.size() != 0) {
-            Map.Entry<String, List<String>> currentNode = getLowestDistanceNode(graph);
+            Map.Entry<String, List<String>> currentNode = getMaxRatioNode(graph, weights);
             graph.remove(currentNode.getKey());
 
             processedApps.add(currentNode.getKey());
@@ -67,7 +68,7 @@ public class SfOptimization {
 
             }
 
-        printResults(processedApps, length);
+//        printResults(processedApps, length);
     }
 
     private static Map.Entry<String, List<String>> getLowestDistanceNode(Map<String, List<String>> unsettled) {
@@ -77,6 +78,21 @@ public class SfOptimization {
             if(entry.getValue().size() < min) {
                 res = entry;
                 min = entry.getValue().size();
+            }
+        }
+        return res;
+    }
+
+    private static Map.Entry<String, List<String>> getMaxRatioNode(Map<String, List<String>> unsettled, Map<String, Integer> weightMap) {
+        double max = -1.0;
+        Map.Entry<String, List<String>> res = null;
+        for(Map.Entry<String, List<String>> entry : unsettled.entrySet()) {
+            int weight = weightMap.get(entry.getKey());
+            int len = entry.getValue().size();
+            double ratio = weight / (len + 0.001);
+            if(ratio > max) {
+                res = entry;
+                max = ratio;
             }
         }
         return res;
